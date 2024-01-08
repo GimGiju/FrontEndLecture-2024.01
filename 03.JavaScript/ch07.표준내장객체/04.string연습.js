f7f26c175dfe04f60ffddaa10ea47900008bef0e
Your Name <you@example.com>
2024-01-08 18:16:46 +0900

    연습문제

--- 03.JavaScript/ch07.표준내장객체/04.string연습.js
+++ 03.JavaScript/ch07.표준내장객체/04.string연습.js
@@ -29,3 +29,44 @@ for (let i = 0; i < numStr.length; i++){
 console.log(countArr);
 
 
+// 2. 디지텔 시계, 00:00 ~ 23:59
+// 하루동안 3이 표시되는 시간은 몇초인가?
+let hour = date.getHours;
+let min = date.getMinutes;
+let sec = date.getSeconds;
+
+for (let i = 0; i <= 1000; i++){
+    h  += i; 
+}
+for (let i = 0; i < 3; i++ ){
+
+}
+
+
+
+
+
+// 3. 두개의 세자리수를 곱해서 나온 결과가 palindrome일때
+// 가장 큰 palindrome 수와 어떤 수를 곱해서 나온 결과인가?
+
+
+
+
+
+//4. C:Workspace/WebProject/03.JavaScript/ch07.표준내장객체/04.String연습.js
+//에서 파일명(04.string연습.js)만 출력하세요.
+
+const find ='C:Workspace/WebProject/03.JavaScript/ch07.표준내장객체/04.String연습.js';
+// let fi = find.slice(49 , 64);
+// console.log(fi);
+let fi = find.split('/').pop();
+console.log(fi);
+// console.log(fi.pop());
+
+// console.log(find.split('/'));
+// console.log(find.pop());
+
+
+
+
+
