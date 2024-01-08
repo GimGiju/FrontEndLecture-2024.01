11a17d7751be0796b206dcb29003796fa1aee272
Your Name <you@example.com>
2024-01-08 09:48:43 +0900

    과제 + 과제 예시 답안

--- 03.JavaScript/ch08.예외처리/Q1.만나이.js
+++ 03.JavaScript/ch08.예외처리/Q1.만나이.js
@@ -1,26 +1,30 @@
 //1. 만 나이를 계산하기 생년, 월, 일을 변수로 설정해서 풀기
 
 
-let mybirthday =new Date( 1998,  3, 31); // 내 생일
+function myage(myage1){
+    let biryear = birthDate.getFullyear();//생일년도
+    let birmon = birthDate.getFullyear(); // 생일 월
+    let birdat = birthDate.getFullyear(); // 생일 일
 
-function myage(manage){
-    let yea = new Date() ;  // 현재 연도
+    let currentDate = new Date();   //현재 날짜
+    let currentyear = currentDate.getFullyear(); //현재 년도
+    let currentmon = currentDate.getFullyear();  //현재 월
+    let currentdat = currentDate.getFullyear();  //현재 일
 
-    let bir = birthdayDate.getFullYear() ;//생년
+    let mage = currentyear - biryear; // 만 나이
 
-    let mon = todaymonth.getFullMonth() ; //이번달
+    if(birmon < currentmon){
+        mage--
+    } //현재 생일과 현재 월 비교
+    else if(currentmon === birmon && currentdat < birdat){
+        mage--
+    }
 
-    let dat = nowdate.getFullDate()  ;      // 오늘 날짜 
-
-    let man = yea - bir  // 만나이
-    if(mon <  ){}
-         
-
-
+    return mage;
     
-
-
-
 }
-console.log(`현재 날짜는${bir}년 ${mon}월${dat}입니다.`)
-
+let birthDate = new Date('1998-03-31');// 내 생일
+let mage = myage(myage1);
+console.log(`현재 날짜는 ${currentyear}년/${currentmon}월/${currentdat}일 입니다. `)
+console.log(`제 생일은 ${biryear}년 /${birmon}월/ ${birdat}일 입니다.`);
+console.log(`제 만나이는 ${mage}입니다`);
--- 03.JavaScript/ch08.예외처리/Q2.굿바이.js
+++ 03.JavaScript/ch08.예외처리/Q2.굿바이.js
@@ -5,3 +5,16 @@
 //그렇다. 내년의 연도인 2024가 올해 연도의 끝 두 자리인 23으로 나누어 떨어진다! 
 //다음에도 이런 연도가 오려면 6년 뒤인 2029/2030년이 되어야 한다.
 //양의 정수 N이 주어진다. N년과 + 1년이 이러한 조건을 만족하는 관계인지 판별하는 프로그램을 작성하라.
+
+
+function isDicisible(year){
+    let year1 = year + 1;
+    let year2 = year % 100;
+    
+    for(let i = 2023; i <= 2040; i++){
+      if(isDicisible(i))
+          console.log('good');
+          else
+          console.log('bye')
+    }
+  }
